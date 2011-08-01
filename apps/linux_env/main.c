bf37d433436b55190682eea82cf0e67f19a53924
Dan Schatzberg <schatzberg.dan@gmail.com>
2011-08-01 14:23:16 -0400

    changed the linux environment to spawn number of threads = num virtual cpus in linux wq

--- apps/linux_env/main.c
+++ apps/linux_env/main.c
@@ -1,7 +1,7 @@
 #include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-#include "../../libs/ebb/lrt/ulnx/EBBConst.h"
 #include "../../libs/base/types.h"
 #include "../../libs/cobj/cobj.h"
 #include "../../libs/ebb/EBBTypes.h"
@@ -11,27 +11,35 @@
 #include "../../libs/ebb/EBBMemMgrPrim.h"
 #include "../../libs/ebb/EBBCtr.h"
 #include "../../libs/ebb/EBBCtrPrim.h"
+#include "../../libs/ebb/EBBCtrPrimDistributed.h"
 #include "../../libs/ebb/clrBTB.h"
 #include <sys/time.h>
+#include <unistd.h>
+#include <sched.h>
 
 pthread_key_t ELKey;
 pthread_barrier_t ELBarrier;
+pthread_t *threads;
 
-long sec[LRT_MAX_VPS];
-long usec[LRT_MAX_VPS];
+long *sec;
+long *usec;
 
-EBBCtrPrimId c;
+EBBCtrPrimDistributedId c;
 
 void *vp_thread(void *arg) {
   int i;
   uval v;
   EBBRC rc;
   struct timeval tv1, tv2;
+  cpu_set_t set;
   
   pthread_setspecific(ELKey, arg);
+  CPU_ZERO(&set);
+  CPU_SET(LRTEBBMyEL(), &set);
+  pthread_setaffinity_np(threads[LRTEBBMyEL()], sizeof(cpu_set_t), &set);
   pthread_barrier_wait(&ELBarrier);
   gettimeofday(&tv1, NULL);
-  for(i = 0; i < 100000000; i++) {
+  for(i = 0; i < 1000000000; i++) {
     rc = EC(c)->inc(EB(c));
   }
   gettimeofday(&tv2, NULL);
@@ -45,23 +53,27 @@ void *vp_thread(void *arg) {
 		
 int main () {
   long i;
-  pthread_t threads[LRT_MAX_VPS];
-  cpu_set_t set;
+  long numCpu = sysconf(_SC_NPROCESSORS_ONLN);
+/*   long numCpu = 1; */
+
+  threads = malloc(sizeof(pthread_t)*numCpu);
+  sec = malloc(sizeof(long)*numCpu);
+  usec = malloc(sizeof(long)*numCpu);
 
   EBBMgrPrimInit();
   EBBMemMgrPrimInit();
 
-  pthread_barrier_init(&ELBarrier, NULL, LRT_MAX_VPS);
+  pthread_barrier_init(&ELBarrier, NULL, numCpu);
   pthread_key_create(&ELKey, NULL);
-  EBBCtrPrimSharedCreate(&c);
-  for (i = 0; i < LRT_MAX_VPS; i++) {
+  EBBCtrPrimDistributedCreate(&c);
+  for (i = 0; i < numCpu; i++) {
     sec[i] = 0;
     usec[i] = 0;
     pthread_create(&threads[i], NULL, vp_thread, (void *)i);
   }
-  for (i = 0; i < LRT_MAX_VPS; i++) {
+  for (i = 0; i < numCpu; i++) {
     pthread_join(threads[i], NULL);
-    printf("%ld.%ld\n",
+    printf("%ld.%.6ld\n",
 	   sec[i], usec[i]);
   }
 }
