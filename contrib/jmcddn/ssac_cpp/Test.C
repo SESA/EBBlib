d46aa06c723fd4491269eed96bc87f7007f004fb
Jim Cadden <jmcadden@bu.edu>
2012-02-17 13:41:19 -0500

    threads binding works on single-core VM.... will try it on some actual cores in the morning.

--- contrib/jmcddn/ssac_cpp/Test.C
+++ contrib/jmcddn/ssac_cpp/Test.C
@@ -20,6 +20,99 @@ testPThreadFunc(void *args)
   return  (void *) (long) arg->test->worker(arg->index);
 }
 
+int
+static num_phys_cores()
+{
+#ifdef __APPLE__
+  // based on doc I could find on net about OSX/mach internals
+  int mib[4], numcores;
+  size_t len, size;
+  
+  len = 4;
+  sysctlnametomib("hw.physicalcpu_max", mib, &len);
+
+  size = sizeof(numcores);
+  if (sysctl(mib, len, &numcores, &size, NULL, 0)==-1) {
+	perror("sysctl");
+	return -1;
+  }
+  return numcores;
+#else 
+  return sysconf(_SC_NPROCESSORS_ONLN);
+#endif
+}
+
+#ifndef __APPLE__
+  struct linux_thread_init_arg {
+    void *(*func)(void *);
+    void *args;
+    int proc; 
+  };
+ 
+void *
+linux_thread_init(void *arg)//FIXME: everthing is named args!
+{
+  struct linux_thread_init_arg *a = (struct linux_thread_init_arg *)arg;
+  void *(*func)(void *) = a->func;
+  int processor = a->proc;
+  cpu_set_t mask;
+  // Pin process to cpu
+  CPU_ZERO( &mask );
+  CPU_SET(processor, &mask);
+  if (sched_setaffinity(0, sizeof(mask), &mask) == -1) {
+    perror("ERROR: Could not set CPU Affinity, exiting...\n");
+    exit(-1);
+  }
+  return func(a->args);
+}
+#endif
+
+pthread_t
+create_bound_thread(pthread_t *tid, int id,  void *(*func)(void *), void *arg)
+{ 
+  int numcores, pid, rc;
+  numcores = num_phys_cores();
+  pid = id % numcores;  
+  
+  if (id < 0 || id >= numcores) return -1;
+
+#ifdef __APPLE__
+  // Apple bind code 
+  thread_affinity_policy_data_t affinityinfo;
+  rc = pthread_create_suspended_np(&tid, NULL, func, arg);// TODO: verify value of arg is correct for OSX
+  if (rc != 0) {
+    perror("pthread_create_suspended_np");
+    return -1;
+  }
+  affinityinfo.affinity_tag = pid+1; // why +1? 
+  rc = thread_policy_set(pthread_mach_thread_np(tids[id]), 
+			 THREAD_AFFINITY_POLICY,
+			 &affinityinfo,
+			 THREAD_AFFINITY_POLICY_COUNT);
+  if (rc != KERN_SUCCESS) {
+    perror("thread_policy_set");
+    return -1;
+  }
+  thread_resume(pthread_mach_thread_np(tids[id]));
+#else
+
+  // Linux bind code here
+  struct linux_thread_init_arg *lnxargs;
+  lnxargs->func = func;
+  lnxargs->args = arg;
+  lnxargs->proc = pid;
+  pthread_t temp =0;
+  rc = pthread_create(&temp, NULL, linux_thread_init, (void *)lnxargs);
+  if (rc != 0) {
+    perror("pthread_create");
+    return -1;
+  }
+
+#endif
+  return 0;
+}
+
+
 Test::Test(int n) :  bar(n), numWorkers(n), iterations(1)
 {
   wargs = (struct Test::WArgs *)
@@ -49,16 +142,19 @@ Test::doWork() {
       args[i].id = i;
       args[i].index = (j*numWorkers)+i;
       args[i].test = this; // TODO: look up behavior of 'this'
-      if ( pthread_create( &(args[i].tid), NULL, 
-                           testPThreadFunc, (void *)&(args[i])) != 0) {
+
+      if ( pthread_create( &(args[i].tid), NULL, testPThreadFunc, (void *)&(args[i])) != 0) {
         perror("pthread_create");
         exit(-1);
-      }
+      } 
+     /*if ( create_bound_thread( &(args[i].tid), i, testPThreadFunc, (void *)&(args[i])) < 0) {
+        perror("create_bound_thread");
+        exit(-1);
+      }*/
     }
     for (i = 0; i<numWorkers; i++)
       pthread_join(args[i].tid, NULL );
-    free(args); 
-
+      free(args); 
   }
   return 0;
 }
