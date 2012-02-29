e5218585f55b47b749e28c8b8bdf1e1a74b93195
Jonathan Appavoo <jappavoo@bu.edu>
2012-02-29 18:38:00 -0500

    Added code to start the process of resurrecting the ethernet code
    NOTE:  the way that libpcap was added to the link line needs to be fixed so that it is only reference when linking the ethtst app.

---
+++ apps/ethtst/ethtst.c
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2011 by Project SESA, Boston University
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#include <config.h>
+#include <stdint.h>
+
+#include <l0/lrt/types.h>
+#include <l0/cobj/cobj.h>
+#include <lrt/io.h>
+#include <l0/lrt/pic.h>
+#include <l0/lrt/trans.h>
+#include <l0/types.h>
+#include <l0/sys/trans.h>
+#include <lrt/assert.h>
+#include <l0/cobj/CObjEBB.h>
+#include <l0/EBBMgrPrim.h>
+#include <l0/cobj/CObjEBBUtils.h>
+#include <l0/cobj/CObjEBBRoot.h>
+#include <l0/cobj/CObjEBBRootMulti.h>
+#include <l0/cobj/CObjEBBRootMultiImp.h>
+#include <l0/EventMgrPrim.h>
+#include <l0/EventMgrPrimImp.h>
+#include <l0/lrt/pic.h>
+#include <l0/MemMgr.h>
+#include <l0/MemMgrPrim.h>
+#include <net/EthTypeMgr.h>
+#include <net/EthMgr.h>
+#include <net/EthMgrPrim.h>
+
+EthMgrId theEthMgr;
+
+EBBRC ebbmain(void)
+{
+  EBBRC rc;
+
+  EBB_LRT_printf("%s: START\n", __func__);
+
+  rc = EthMgrPrimCreate(&theEthMgr, "eth1");
+  EBBRCAssert(rc);
+
+  EBB_LRT_printf("%s: END\n", __func__);
+  
+  return EBBRC_OK;
+}
