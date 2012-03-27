031eb22b04d2355904dc369d05abc24b3caf39c2
James Cadden <jmcadden@bu.edu>
2012-03-27 13:03:14 -0400

    Moved Directory Reference to the wiki, removed /doc/ folder

--- apps/ssac/ssac.c
+++ apps/ssac/ssac.c
@@ -22,32 +22,37 @@
 
 #include <config.h>
 #include <stdint.h>
-
-#include <l0/lrt/types.h>
-#include <l0/cobj/cobj.h>
-#include <lrt/io.h>
-#include <l0/lrt/pic.h>
-#include <l0/lrt/trans.h>
-#include <l0/types.h>
-#include <l0/sys/trans.h>
-#include <lrt/assert.h>
+//
 #include <l0/cobj/CObjEBB.h>
-#include <l0/EBBMgrPrim.h>
 #include <l0/cobj/CObjEBBUtils.h>
 #include <l0/cobj/CObjEBBRoot.h>
 #include <l0/cobj/CObjEBBRootMulti.h>
 #include <l0/cobj/CObjEBBRootMultiImp.h>
+#include <l0/cobj/cobj.h>
+//
+#include <l0/EBBMgrPrim.h>
 #include <l0/EventMgrPrim.h>
 #include <l0/EventMgrPrimImp.h>
-#include <l0/lrt/pic.h>
 #include <l0/MemMgr.h>
 #include <l0/MemMgrPrim.h>
+//
+#include <l0/lrt/types.h>
+#include <l0/lrt/pic.h>
+#include <l0/lrt/trans.h>
+#include <l0/types.h>
+#include <l0/sys/trans.h>
+//
+#include <lrt/io.h>
+#include <lrt/assert.h>
+//
 #include <l1/App.h>
 
+/* define a C-language EBB, named SSAC */
 CObject(SSAC) {
-  CObjInterface(App) *ft;
+  CObjInterface(App) *ft; //
 };
 
+/* debug dump envoirnment vars */
 static void
 dumpArgsAndEnviron(int argc, char **argv, char **environ)
 {
@@ -57,6 +62,7 @@ dumpArgsAndEnviron(int argc, char **argv, char **environ)
 						  i, environ[i]);
 }
 
+/* EBB start function */
 EBBRC
 SSAC_start(AppRef _self, int argc, char **argv,
 		 char **environ)
@@ -67,9 +73,11 @@ SSAC_start(AppRef _self, int argc, char **argv,
   return EBBRC_OK;
 }
 
+/* Specify EBB interface */
 CObjInterface(App) SSAC_ftable = {
   .start = SSAC_start
 };
 
+/* Define Application */
 APP(SSAC);
 
