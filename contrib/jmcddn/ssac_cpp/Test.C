d054dc15d372f7dc3df149ad988c1e0c4d2b885d
James Cadden <jmcadden@bu.edu>
2012-03-28 14:22:44 -0400

    disabled bs barrier, added pthread barrier

--- contrib/jmcddn/ssac_cpp/Test.C
+++ contrib/jmcddn/ssac_cpp/Test.C
@@ -32,27 +32,25 @@ static num_phys_cores()
   // based on doc I could find on net about OSX/mach internals
   int mib[4], numcores;
   size_t len, size;
-  
   len = 4;
   sysctlnametomib("hw.physicalcpu_max", mib, &len);
-
   size = sizeof(numcores);
   if (sysctl(mib, len, &numcores, &size, NULL, 0)==-1) {
 	perror("sysctl");
 	return -1;
   }
   return numcores;
-#else 
+#else // if LINUX/UNIX
   return sysconf(_SC_NPROCESSORS_ONLN);
 #endif
 }
 
 #ifndef __APPLE__
-  struct linux_thread_init_arg {
-    void *(*func)(void *);
-    void *args;
-    int proc; 
-  };
+struct linux_thread_init_arg {
+  void *(*func)(void *);
+  void *args;
+  int proc;
+};
 
 void *
 linux_thread_init(void *arg)
@@ -60,39 +58,38 @@ linux_thread_init(void *arg)
   struct linux_thread_init_arg *a = (struct linux_thread_init_arg *)arg;
   void *(*func)(void *) = a->func;
   int processor = a->proc;
-  cpu_set_t mask; 
-  size_t mask_size; 
+  cpu_set_t mask;
   // Pin process to cpu
   CPU_ZERO( &mask );
   CPU_SET(processor, &mask);
-  if (pthread_setaffinity_np(pthread_self(), sizeof(mask), &mask) == -1) {//sched_setaffinity(0, mask_size,mask) == -1) {
+  if (pthread_setaffinity_np(pthread_self(), sizeof(mask), &mask) == -1) {
     perror("ERROR: Could not set CPU Affinity, exiting...\n");
     exit(-1);
   }
-  return func(a->args); 
+  return func(a->args);
 }
 #endif
 
 int
 create_bound_thread(pthread_t *tid, int id,  void *(*func)(void *), void *arg)
-{ 
-  int numcores, pid, rc;
+{
+    int numcores, pid, rc;
   numcores = num_phys_cores();
-  pid = id % numcores;  
-  
+  pid = id % numcores;
+
   if ((id < 0)) return -1;
 
 #ifdef __APPLE__
-  // Apple bind code 
+  // Apple bind code
   thread_affinity_policy_data_t affinityinfo;
   rc = pthread_create_suspended_np(tid, NULL, func, arg);// TODO: verify value of arg is correct for OSX
   if (rc != 0) {
     perror("pthread_create_suspended_np");
     return -1;
   }
-  affinityinfo.affinity_tag = pid+1; // why +1? 
+  affinityinfo.affinity_tag = pid+2; // FIXME: confirm +2
 
-  rc = thread_policy_set(pthread_mach_thread_np(*tid), 
+  rc = thread_policy_set(pthread_mach_thread_np(*tid),
 			 THREAD_AFFINITY_POLICY,
 			 (int *)&affinityinfo,
 			 THREAD_AFFINITY_POLICY_COUNT);
@@ -103,9 +100,9 @@ create_bound_thread(pthread_t *tid, int id,  void *(*func)(void *), void *arg)
   thread_resume(pthread_mach_thread_np(*tid));
 #else
 
-  // Linux bind code 
-  struct linux_thread_init_arg *lnxargs; 
-  lnxargs = (linux_thread_init_arg *)malloc(sizeof(linux_thread_init_arg));// FIXME: memory leak... 
+  // Linux bind code
+  struct linux_thread_init_arg *lnxargs;
+  lnxargs = (linux_thread_init_arg *)malloc(sizeof(linux_thread_init_arg));// FIXME: memory leak...
   lnxargs->func = func;
   lnxargs->args = arg;
   lnxargs->proc = pid;
@@ -123,33 +120,34 @@ create_bound_thread(pthread_t *tid, int id,  void *(*func)(void *), void *arg)
 Test::Test(int n) :  bar(n), numWorkers(n), iterations(1)
 {
   wargs = (struct Test::WArgs *)
-  malloc(sizeof(struct Test::WArgs) * numWorkers);  
+  malloc(sizeof(struct Test::WArgs) * numWorkers);
   tassert((wargs != NULL), ass_printf("malloc failed\n"));
 }
 
 Test::Test(int n, int m) :  bar(n), numWorkers(n), iterations(m)
 {
   wargs = (struct Test::WArgs *)
-  malloc(sizeof(struct Test::WArgs) * (numWorkers * iterations));  
+  malloc(sizeof(struct Test::WArgs) * (numWorkers * iterations));
   tassert((wargs != NULL), ass_printf("malloc failed\n"));
 }
 
 Test::Test(int n, int m, bool p) :  bar(n), numWorkers(n), iterations(m), bindThread(p)
 {
+  pthread_barrier_init(&bar2, NULL, n);
   wargs = (struct Test::WArgs *)
-  malloc(sizeof(struct Test::WArgs) * (numWorkers * iterations));  
+  malloc(sizeof(struct Test::WArgs) * (numWorkers * iterations));
   tassert((wargs != NULL), ass_printf("malloc failed\n"));
 }
 
-EBBRC 
+EBBRC
 Test::doWork() {
   struct TestPThreadArgs *args;
   int i,j;
 
   // test iteration loop
-  for(j=0; j<iterations; j++){ 
+  for(j=0; j<iterations; j++){
     args = (struct TestPThreadArgs *)
-    malloc(sizeof(struct TestPThreadArgs) * numWorkers);  
+    malloc(sizeof(struct TestPThreadArgs) * numWorkers);
     tassert((args != NULL), ass_printf("malloc failed\n"));
    for (i=0; i<numWorkers; i++) {
 //    TRACE("creating thread #%d\n", i);
@@ -161,8 +159,8 @@ Test::doWork() {
       if ( create_bound_thread( &(args[i].tid), i, testPThreadFunc, (void *)&(args[i])) < 0) {
 	    perror("pthread_create");
 	    exit(-1);
-      } 
-    }else{ 
+      }
+    }else{
       if ( pthread_create( &(args[i].tid), NULL, testPThreadFunc, (void *)&(args[i])) != 0) {
 	    perror("create_bound_thread");
 	    exit(-1);
@@ -171,7 +169,7 @@ Test::doWork() {
   }
     for (i = 0; i<numWorkers; i++)
       pthread_join(args[i].tid, NULL );
-      free(args); 
+      free(args);
   }
   return 0;
 }
@@ -181,14 +179,26 @@ Test::worker(int id)
 {
   struct WArgs *args = &(wargs[id]);
 
-  bar.enter();
+  int rc = pthread_barrier_wait(&bar2);
+  if(rc != 0 && rc != PTHREAD_BARRIER_SERIAL_THREAD)
+  {
+    printf("Could not wait on barrier\n");
+    exit(-1);
+  }
+
+  // bar.enter();
 
   args->start = now();
   work(id);
   args->end = now();
 
-  bar.enter();
-
+ // bar.enter();
+  rc = pthread_barrier_wait(&bar2);
+  if(rc != 0 && rc != PTHREAD_BARRIER_SERIAL_THREAD)
+  {
+    printf("Could not wait on barrier\n");
+    exit(-1);
+  }
   return 0;
 }
 
@@ -208,6 +218,6 @@ Test::end()
       index = (i*numWorkers)+j;
       printf("%d, %d, %llu, %llu, %llu\n", i, j, wargs[index].start, wargs[index].end, wargs[index].end - wargs[index].start);
     }
-  
+
   return 0;
 }
