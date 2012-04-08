6888e501b4c4dd8134607583058af429786c6a58
James Cadden <jmcadden@bu.edu>
2012-04-07 22:14:04 -0400

    added C++ helloworld app

---
+++ apps/helloworldcpp/helloworld.cpp
@@ -0,0 +1,80 @@
+/*
+ * Copyright (C) 2011 by Project SESA, Boston University
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+#include <config.h>
+#include <stdint.h>
+#include <lrt/assert.h>
+#include <lrt/io.h>
+#include <l0/lrt/pic.h>
+#include <l0/lrt/trans.h>
+#include <l0/lrt/types.h>
+#include <l0/types.h>
+#include <l0/sys/trans.h>
+#include <l0/cobj/cobj.h>
+#include <l0/cobj/CObjEBB.h>
+#include <l0/cobj/CObjEBBRoot.h>
+#include <l0/cobj/CObjEBBRootMulti.h>
+#include <l0/cobj/CObjEBBRootMultiImp.h>
+#include <l0/EBBMgrPrim.h>
+#include <l0/EventMgrPrim.h>
+#include <l0/EventMgrPrimImp.h>
+#include <l0/MemMgr.h>
+#include <l0/MemMgrPrim.h>
+#include <l0/cplus/CPlusEBB.H>
+#include <l0/cplus/CPlusEBBRoot.H>
+#include <l0/cplus/CPlusEBBRootShared.H>
+#include <l1/App.h>
+
+class HW;
+typedef HW **HWId;
+
+class HW : CPlusEBB {
+  public:
+  EBBRC printMsg ();
+};
+
+EBBRC
+HW::printMsg()
+{
+  EBB_LRT_printf("Hello world!\n");
+  return EBBRC_OK;
+}
+
+/* ******************************** */
+
+EBBRC
+HelloWorld_start(AppRef _self, int argc, char **argv,
+		 char **environ)
+{
+  HW MyHelloWorld;
+  MyHelloWorld.printMsg();
+  return EBBRC_OK;
+}
+
+CObject(HelloWorld) {
+  CObjInterface(App) *ft;
+};
+
+CObjInterface(App) HelloWorld_ftable = {HelloWorld_start};
+
+extern "C" {
+  APP(HelloWorld);
+}
